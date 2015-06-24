b65303aa1e6017735adc8697809a69c442639acd
Tom Knight <tktom@hotmail.com>
2015-06-24 13:06:57 +0100

    logout button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
-  def new    
+  def new
+    redirect_to root_path if session[:admin]
   end
 
   def create
     # binding.pry
     session[:admin] = params[:password] == ENV["ADMIN_PASSWORD"]
     redirect_to root_path
+  end
+
+  def logout
+    session[:admin] = false
+    redirect_to login_path
   end
 
 end
