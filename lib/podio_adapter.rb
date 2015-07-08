8f229688a873bb4771b6c12be8f3e28fe0e2da3a
Tom Knight <tktom@hotmail.com>
2015-07-08 14:35:00 +0100

    fix podio auth params

--- lib/podio_adapter.rb
+++ lib/podio_adapter.rb
@@ -1,9 +1,9 @@
 require "podio"
 
 class PodioAdapter
-  
+
   def initialize
-    Podio.setup(ENV["PB_PODIO_CLIENT_ID"], ENV["PB_PODIO_CLIENT_SECRET"])
+    Podio.setup(api_key: ENV["PB_PODIO_CLIENT_ID"], api_secret: ENV["PB_PODIO_CLIENT_SECRET"])
     Podio.client.authenticate_with_credentials(ENV["PB_PODIO_USERNAME"], ENV["PB_PODIO_PASSWORD"])
   end
 
