d9baac629f9f060486335a06d7bc5372098b236c
Tom Knight <tktom@hotmail.com>
2015-07-14 16:04:27 +0100

    extract issue & create podio item

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -3,6 +3,13 @@ class GithubController < ApplicationController
   skip_before_action :require_login, only: [:create]
 
   def create
+    # binding.pry
+    Log.create(
+      sender: "GithubController", 
+      message: params[:github][:action],
+      status: "success"
+      )
+    GithubAdapter.new.extract_issue(params)
     render nothing: true, status: 200
   end
 
--- lib/github_adapter.rb
+++ lib/github_adapter.rb
@@ -13,4 +13,17 @@ class GithubAdapter
     client.issues.create user: USER, repo: REPO, title: title, body: body
   end
 
+  def extract_issue(params)
+    # binding.pry
+    PodioAdapter.new
+    Podio::Item.create(12885408, fields: {
+      title: params[:issue][:title],
+      body: params[:issue][:body],
+      assignee: params[:issue][:assignee],
+      "created-by" => params[:issue][:user][:login],
+      state: params[:issue][:state],
+      "github-id" => params[:issue][:number].to_s
+    })
+  end
+
 end
