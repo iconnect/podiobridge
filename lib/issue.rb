6dfaab557deb61547f533ada19955332816ba988
Tom Knight <tktom@hotmail.com>
2015-07-16 15:59:08 +0100

    refactor podio_to_hash method

--- lib/issue.rb
+++ lib/issue.rb
@@ -22,13 +22,16 @@ class Issue
   end
 
   def podio_to_hash
-    title = item[:fields].select { |x| x["external_id"] == "title" }.flat_map{ |x| x["values"] }.map { |x| x["value"] }.first
-    body = item[:fields].select { |x| x["external_id"] == "body" }.flat_map{ |x| x["values"] }.map { |x| x["value"] }.first
-    # assignee = item[:fields].select { |x| x["external_id"] == "assignee" }.flat_map{ |x| x["values"] }.map { |x| x["value"] }.first
-    # created_by = item[:fields].select { |x| x["external_id"] == "created-by" }.flat_map{ |x| x["values"] }.map { |x| x["value"] }.first
-    state = item[:fields].select { |x| x["external_id"] == "state" }.flat_map{ |x| x["values"] }.map { |x| x["value"]["text"] }.first
-    github_id = item[:fields].select { |x| x["external_id"] == "github-id" }.flat_map{ |x| x["values"] }.map { |x| x["value"] }.first
+    binding.pry
+    title = field("title").value
+    body = field("body").value
+    # assignee = field("assignee").value
+    # created_by = field("created-by").value
+    state = field("state").value["text"]
+    github_id = field("github-id").value
   
+    i = field("title").value
+
 
     state ||= nil
     github_id ||= nil
@@ -44,5 +47,15 @@ class Issue
     return item_hash
   end
 
+  def field(name)
+    Field.new item[:fields].select { |x| x["external_id"] == name }.first
+  end  
+
+  class Field < OpenStruct
+    def value
+      values.map { |x| x["value"] }.first
+    end
+
+  end
 
 end
