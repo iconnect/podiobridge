4d0a283c30aa7a97c7e61c56be0d30a4f172775a
Tom Knight <tktom@hotmail.com>
2015-07-17 11:31:26 +0100

    podio authenticate with app

--- lib/podio_adapter.rb
+++ lib/podio_adapter.rb
@@ -4,7 +4,8 @@ class PodioAdapter
 
   def initialize
     Podio.setup(api_key: ENV["PB_PODIO_CLIENT_ID"], api_secret: ENV["PB_PODIO_CLIENT_SECRET"])
-    Podio.client.authenticate_with_credentials(ENV["PB_PODIO_USERNAME"], ENV["PB_PODIO_PASSWORD"])
+    # Podio.client.authenticate_with_credentials(ENV["PB_PODIO_USERNAME"], ENV["PB_PODIO_PASSWORD"])
+    Podio.client.authenticate_with_app(ENV["PB_PODIO_APP_ID"], ENV["PB_PODIO_APP_TOKEN"])
   end
 
   def verify_hook(hook_id, code)
