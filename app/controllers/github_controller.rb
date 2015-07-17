9b8707d5534995e9c2881505bb4031b905c2e5fd
Tom Knight <tktom@hotmail.com>
2015-07-17 12:01:07 +0100

    check if user or podiobridge fired webhook

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -8,18 +8,21 @@ class GithubController < ApplicationController
       message: params[:github][:action],
       status: "info"
       )
+
+    render nothing: true, status: 200 and return if params[:sender]["login"] == "podiobridge"
+
     pa = PodioAdapter.new
     item_hash = Issue.new(params).github_to_hash
 
     action = params[:github][:action]
-    podio_item = pa.find_item(12885408, "github-id", params[:issue][:number].to_s)
-
+    
     case action
     when "opened"
-      pa.create_item(12885408, item_hash) unless podio_item
+      pa.create_item(12885408, item_hash)
     when "created"
       #comment created
     else
+      podio_item = pa.find_item(12885408, "github-id", params[:issue][:number].to_s)
       pa.update_item(podio_item.item_id, item_hash)
     end
 
